b675ddb6514e069d3ce3d2aac5a51ffc108ef1eb
Chanwoo18 <sa0505sa@mokpo.ac.kr>
2022-11-22 16:11:10 +0900

    화면레이아웃

--- project/app.py
+++ project/app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=3000)
