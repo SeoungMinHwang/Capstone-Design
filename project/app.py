78bbc9641d662d0fbb752bb9d5dd71a0e439264e
hunsoo <hunsoo0224@gmail.com>
2022-11-23 14:59:03 +0900

    성민, 헌수 flask opencv webcam 적용

--- project/app.py
+++ project/app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
+camera = cv2.VideoCapture('http://192.168.35.226:8000/stream.mjpg')
+
+def gen_frames():  
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
@@ -10,3 +27,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
+    
+
+
