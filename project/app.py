34005b00354adf1271fae84a29b6e31c3784398d
Chanwoo18 <sa0505sa@mokpo.ac.kr>
2022-11-24 16:27:17 +0900

    화면분할테스트

--- project/app.py
+++ project/app.py
@@ -29,6 +29,9 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/all_cctv')
+def all_cctv():
+    return render_template('all_cctv.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
