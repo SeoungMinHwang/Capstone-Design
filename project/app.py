e687cf98c4cab45029f6e798ede06db2a154ea46
Chanwoo18 <sa0505sa@mokpo.ac.kr>
2022-11-24 14:50:42 +0900

    로그인 페이지

--- project/app.py
+++ project/app.py
@@ -24,6 +24,10 @@ def video_feed():
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
