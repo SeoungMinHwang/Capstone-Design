88136af89c8a3be7dfbbefad103d067e80c25381
Chanwoo18 <sa0505sa@mokpo.ac.kr>
2022-11-30 15:16:31 +0900

    update

--- project/app.py
+++ project/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, redirect, url_for
 import cv2,camera
 
 app = Flask(__name__)
-camera1,camera2,camera3 = camera.camera_start()
+camera1,camera2,camera3,camera4,camera5,camera6 = camera.camera_start()
 
 # camera = cv2.VideoCapture('http://192.168.35.226:8000/stream.mjpg')
 # camera1 = cv2.VideoCapture(0)
@@ -28,29 +28,39 @@ def video_feed(cctv_section):
         return Response(gen_frames(camera2), mimetype='multipart/x-mixed-replace; boundary=frame')
     elif cctv_section=='목포대1':
         return Response(gen_frames(camera3), mimetype='multipart/x-mixed-replace; boundary=frame')
+    elif cctv_section=='목포대2':
+        return Response(gen_frames(camera3), mimetype='multipart/x-mixed-replace; boundary=frame')
+    elif cctv_section=='하당1':
+        return Response(gen_frames(camera3), mimetype='multipart/x-mixed-replace; boundary=frame')
+    elif cctv_section=='하당2':
+        return Response(gen_frames(camera3), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 # CCTV상세정보
 @app.route('/detail')
 def detail():
     sec = request.args.get('section')
     return render_template('detail.html', sec=sec)
 # 로그인페이지
+@app.route('/login')
 @app.route('/')
 def login():
     return render_template('login.html')
 # 전체CCTV
 @app.route('/all_cctv')
 def all_cctv():
-    cctv_list = ['남악1','남악2','목포대1','목포대2','하당1','하당2',]
+    cctv_list = ['남악1','남악2','목포대1','목포대2','하당1','하당2']
     return render_template('all_cctv.html',cctv_list=cctv_list)
 
 # 사용자 확인
-# @app.route("/login_confirm",methods=['POST'])
-# def login_confirm():
-#     if (request.form['inputId']=='admin'and request.form['inputPassword']='123'):
-#         session['id'] = id
-#         return redirect(url_for('all_cctv'))
-#     else:
-#         return redirect(url_for('/'))
+@app.route("/login_confirm",methods=['POST'])
+def login_confirm():
+    inputId = request.form['inputId']
+    inputPassword = request.form['inputPassword']
+    if (inputId=='admin'and inputPassword=='123'):
+        # session['id'] = inputId
+        return redirect(url_for('all_cctv'))
+    else:
+        return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000, threaded=True)
