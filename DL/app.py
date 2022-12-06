8ec30c31b2cbb219cd423604fb318ec2398262f8
hunsoo <hunsoo0224@gmail.com>
2022-12-07 04:00:57 +0900

    fall detection web 적용

--- DL/app.py
+++ DL/app.py
@@ -1,5 +1,5 @@
 # ------------------------------------------
-# 웹기반 streamlit으로 실신자 분류 판별
+# 웹기반 streamlit으로 실신자 탐지, 분류 판별
 # 실행 방법
 # 터미널 창에 streamlit run app.py 입력
 #-------------------------------------------
@@ -9,19 +9,22 @@ from PIL import Image
 import io
 import os
 import pathlib
-
+import shutil
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 learn_inf = load_learner('export.pkl')
 
+import torch
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
+
 def predict_info(img):
     pred, pred_idx, prob = learn_inf.predict(img)
     return (pred, prob[pred_idx])
 
-st.title("Fall Classification")
+st.title("Image Fall Detection, Classification")
 
-st.write("> **이미지 분류 모델**")
+st.write("> **이미지 탐지 모델과 분류 모델**")
 uploaded_file = st.file_uploader('', type=['png', 'jpg'])
 tmp_loc = False
 
@@ -34,12 +37,29 @@ if uploaded_file is not None:
     out.close()
 
     image_local = Image.open(tmp_loc)
-    _, c1, _ = st.columns(3)
+    c1, c2 = st.columns(2)
     with c1:
+        st.image(image_local, caption='')
+        if st.button('탐지하기'):
+            result=model([image_local], size=640)
+            print(result.save())
+            
+            det_img = Image.open('./runs/detect/exp/result.jpg')
+            st.image(det_img,caption='')
+    
+    
+            shutil.rmtree('./runs')
+    with c2:
         st.image(image_local, caption='')
         if st.button('분류하기'):
             pred, prob = predict_info(tmp_loc)
             st.success(f"분류: {pred}")
             st.success(f"확률: {(prob*100):0.2f}%")
+            
+    
     os.remove(tmp_loc)
+
+
+
+
 
