451f52943baff541cc4b375414ac97a1d1b8fc1a
hunsoo <hunsoo0224@gmail.com>
2022-12-06 23:58:07 +0900

    update code (simple)

--- DL/app.py
+++ DL/app.py
@@ -20,16 +20,8 @@ def predict_info(img):
     return (pred, prob[pred_idx])
 
 st.title("Fall Classification")
-st.sidebar.title("이미지 분류하기")
-st.sidebar.write("## Fall Detection")
 
-st.sidebar.write("한번 예측해보기 👋")
-st.sidebar.write("**fall:쓰러짐 | notfallen:안쓰러짐**")
-user_ans = st.sidebar.text_input("컴퓨터가 분류할 것 같은 것을 예측해보세요", value="", max_chars=20)
-
-st.sidebar.write("**당신의 선택**:", user_ans)
-
-st.write("> **컴퓨터는 이미지에 있는 사람이 쓰러졌다고 생각할까요 아니면 안쓰러졌다고 생각할까요**")
+st.write("> **이미지 분류 모델**")
 uploaded_file = st.file_uploader('', type=['png', 'jpg'])
 tmp_loc = False
 
@@ -49,13 +41,5 @@ if uploaded_file is not None:
             pred, prob = predict_info(tmp_loc)
             st.success(f"분류: {pred}")
             st.success(f"확률: {(prob*100):0.2f}%")
-            
-            if pred == user_ans:
-                st.sidebar.success("예측성공")
-            else:
-                st.sidebar.error("예측실패")
-                
     os.remove(tmp_loc)
 
-
-# st.sidebar.write("**결과:**")
