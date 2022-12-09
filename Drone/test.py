0cf8c539f8db7edb38271babea5576f4c7f3be80
리미누 <leeminwoo36741@gmail.com>
2022-12-09 14:24:34 +0900

    드론 연결 세팅 재확인 및 통신확인

--- Drone/test.py
+++ Drone/test.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def drone_but():
     return render_template('drone_but.html')
-
+@app.route('/drone', methods=['GET'])
+def drone_go():
+    return open("Drone/Start_Drone.py")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000, threaded=True)
